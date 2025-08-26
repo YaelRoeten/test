5e66d10f4962b41100ffae079cc1752efbb8e31b
YaelRoeten <yaelroetenn@gmail.com>
2025-08-26 10:45:13 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,35 +1,57 @@
 /**
  * radio.onReceivedNumber(function (receivedNumber) {
  * 
- * if (receivedNumber == 17) {
+ * if (receivedNumber == 18) {
  * 
- * radio.sendNumber(18)
+ * checkpoint_bericht = 1
  * 
  * }
  * 
  * })
  * 
+ * input.onButtonPressed(Button.A, function () {
+ * 
+ * checkpoint_bericht = 0
+ * 
+ * })
+ * 
+ * let checkpoint_bericht = 0
+ * 
  * radio.setGroup(101)
  * 
  * radio.setTransmitPower(7)
  * 
+ * checkpoint_bericht = 0
+ * 
  * basic.forever(function () {
+ * 
+ * while (checkpoint_bericht == 0) {
+ * 
+ * basic.showIcon(IconNames.Sad)
+ * 
+ * radio.sendNumber(17)
+ * 
+ * }
+ * 
+ * radio.setGroup(208)
+ * 
+ * basic.showIcon(IconNames.Happy)
  * 
  * })
  */
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 18) {
-        checkpoint_bericht = 1
+    if (receivedNumber == 17) {
+        radio.sendNumber(18)
+        radio.setTransmitPower(1)
+        radio.setGroup(208)
+    }
+    if (receivedNumber == 69) {
+        radio.setGroup(101)
+        radio.sendNumber(70)
     }
 })
-let checkpoint_bericht = 0
 radio.setGroup(101)
 radio.setTransmitPower(7)
-checkpoint_bericht = 0
 basic.forever(function () {
-    while (checkpoint_bericht == 0) {
-        basic.showIcon(IconNames.Sad)
-        radio.sendNumber(17)
-    }
-    basic.showIcon(IconNames.Happy)
+	
 })
